f59142cd7a02aca5106017ee75a9aa41efbd493a
he <yo@hesh.cc>
2024-06-25 15:05:10 -0300

    feat/chore: enable nushell, tidy codebase

--- hosts/melchior/user.nix
+++ hosts/melchior/user.nix
@@ -5,8 +5,6 @@
     "${base.modules}/home/shell.nix"
   ];
 
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "${user}";
   home.homeDirectory = "/home/${user}";
 
@@ -19,8 +17,6 @@
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     git
     htop
@@ -28,18 +24,6 @@
     ripgrep
     sops
     tree
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   home.file = {
@@ -49,26 +33,10 @@
     };
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/he/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     EDITOR = "vim";
+    SHELL = "${pkgs.nushell}/bin/nu";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
