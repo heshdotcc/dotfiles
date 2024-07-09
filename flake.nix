9f98bcd27b2dd2fe965c02cbdba22c194c39710e
he <yo@hesh.cc>
2024-07-09 18:00:56 -0300

    feat: provide working dynamic hosts

--- flake.nix
+++ flake.nix
@@ -1,38 +1,36 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    nvim.url = "github:heshdotcc/nvim-flake";
-    env.url = "git+file:///home/he/crypt"; 
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    ownpkgs.url = "github:heshdotcc/ownpkgs";
+    env.url = "git+file:///home/he/crypt";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, ... } @inputs:
   let
+    base = { modules = builtins.toString ./. + "/modules"; };
+    hosts = builtins.attrNames inputs.env.hosts;
     user = "he";
-    base = {
-      modules = builtins.toString ./. + "/modules";
-    };
-    specialArgs = { inherit inputs user base; };
-  in
-  {
-    nixpkgs.overlays = [ inputs.nvim.overlay.default ];
-    nixosModules = import ./modules/nixos;
-    homeModules = import ./modules/home; 
-    nixosConfigurations = { 
-      melchior = nixpkgs.lib.nixosSystem {
+
+    generateHostConfig = host: let
+      specialArgs = { inherit inputs base host user; };
+      config = {
         inherit specialArgs;
         modules = [
-          ./hosts/melchior/soft.nix
-          inputs.home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = specialArgs;
-            };
+          ./hosts/host
+          inputs.home-manager.nixosModules.home-manager {
+            home-manager.extraSpecialArgs = specialArgs;
           }
         ];
       };
-    };
+    in config;
+  in {
+    nixosConfigurations = builtins.listToAttrs (builtins.map (host: {
+      name = host;
+      value = nixpkgs.lib.nixosSystem (generateHostConfig host);
+    }) hosts);
   };
 }
