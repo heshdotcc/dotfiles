36a29e35f8affadaa12289edae88c1dc43086ef3
he <yo@hesh.cc>
2024-07-09 17:50:26 -0300

    feat: provide working dynamic hosts

--- flake.nix
+++ flake.nix
@@ -1,32 +1,35 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    ownpkgs.url = "github:heshdotcc/ownpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    nvim.url = "github:heshdotcc/nvim-flake";
     env.url = "git+file:///home/he/crypt"; 
   };
 
-  outputs = { self, ... } @inputs:
+  outputs = { self, nixpkgs, ... }@inputs:
   let
     user = "he";
     base = {
       modules = builtins.toString ./. + "/modules";
     };
     specialArgs = { inherit inputs user base; };
-  in {
-    nixpkgs.overlays = [ inputs.ownpkgs.nvim.overlays.default ];
+  in
+  {
+    nixpkgs.overlays = [ inputs.nvim.overlay.default ];
     nixosModules = import ./modules/nixos;
     homeModules = import ./modules/home; 
     nixosConfigurations = { 
-      melchior = inputs.nixpkgs.lib.nixosSystem {
+      melchior = nixpkgs.lib.nixosSystem {
         inherit specialArgs;
         modules = [
           ./hosts/melchior/soft.nix
-          inputs.home-manager.nixosModules.home-manager {
-            home-manager.extraSpecialArgs = specialArgs;
+          inputs.home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = specialArgs;
+            };
           }
         ];
       };
